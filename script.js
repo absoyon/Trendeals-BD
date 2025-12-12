b576da29dce6625c542afe7567832e84f63db024
absoyon <absayon38@gmail.com>
2025-12-12 21:57:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Website loaded successfully!");
+
+// Example: simple alert for product buttons
+function addToCart(productName) {
+    alert(productName + " has been added to your cart!");
+}
